d90d189bfc518058456152d171d0cb94e3d9d5f6
Elias Bachaalany <elias.bachaalany@gmail.com>
2022-05-24 23:21:08 -0700

    Added a clean and dedicated native plugin template
    You need ida-cmake to build that template plugin

---
+++ test_scripts/trigger-native/plugin_template/main.cpp
@@ -0,0 +1,22 @@
+#include "idasdk.h"
+
+bool main()
+{
+    auto nfuncs = get_func_qty();
+
+    size_t c = 0;
+    for (size_t i = 0; i < nfuncs; ++i)
+    {
+        func_t* func = getn_func(i);
+        if (func->flags & FUNC_LIB)
+            continue;
+
+        ++c;
+        qstring name;
+        get_func_name(&name, func->start_ea);
+        msg("%a: %s\n", func->start_ea, name.c_str());
+    }
+
+    msg("%zu function(s)\n", c);
+	return true;
+}
